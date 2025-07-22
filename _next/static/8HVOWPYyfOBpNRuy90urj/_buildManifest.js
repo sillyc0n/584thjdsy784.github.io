880c4c4ce1087289e4387307e949b5886488e642
sillyc0n <137494122+sillyc0n@users.noreply.github.com>
2025-07-22 09:12:21 +0100

    Updates

---
+++ _next/static/8HVOWPYyfOBpNRuy90urj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/678-080454873942ff45.js","static/css/00a00ae298a73836.css","static/chunks/pages/index-a8bb71c1944fe86d.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
