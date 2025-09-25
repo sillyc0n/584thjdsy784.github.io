faae1e5da6b227c3233ada78f3ff0143d1aba230
sillyc0n <137494122+sillyc0n@users.noreply.github.com>
2025-09-25 16:40:02 +0100

    Updates

--- _next/static/zHPPlR5aW5Nni1zKMhTik/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/421-db95cee1cfbdca7b.js","static/css/6cf7fde4b9f84815.css","static/chunks/pages/index-1a946a1ffdc6ecf0.js"],"/_error":["static/chunks/pages/_error-a59e2db023c5e431.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
